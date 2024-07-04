5e1e516353d265f764279df08b748a852dbbd8b0
Isaacn123 <nsambai72@gmail.com>
2024-07-04 20:28:43 +0300

    reseting password

--- config/util.py
+++ config/util.py
@@ -1,18 +1,22 @@
+from dotenv import load_dotenv
+import os
+# load .env file
+load_dotenv()
 
-app_key = "K005RVRokrCOJcQ9tSDLq8aDHajziKM"
-app_key_ID = "005daaffbb3b1180000000002"
-bucket_name = "mc-upload-bk"
+app_key = os.getenv('APP_KEY') 
+app_key_ID =  os.getenv('APP_KEY_ID') 
+bucket_name = os.getenv('BUCKET_NAME') 
 
 # config.py
 class Config:
-    JWT_SECRET = '90cf9174c60f0c77dd6706df7176a155'
+    JWT_SECRET = os.getenv('JWT_SECRET')
     RESET_TOKEN_EXPIRATION = 1  # Token expiration time in hours
 
     # Flask-Mail configuration
     MAIL_SERVER = 'pro.turbo-smtp.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'info@yeshualifeug.com'
-    MAIL_PASSWORD = 'r4rqU2Xa'
-    MAIL_DEFAULT_SENDER = 'info@yeshualifeug.com'
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD') 
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
 
